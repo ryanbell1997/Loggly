67a77ac979891b6a8a4fe67e25ef8d12bc606d06
Ryan Bell <ryan.bell1997@hotmail.co.uk>
2021-10-16 10:37:54 +0100

    FrontEndReady

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+
+  const [logs, setLogs] = useState<any>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/logs').then(response => {
+      console.log();
+      setLogs(response.data);
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon='users' content='Loggly.io'/>
+        <List>
+          {logs.map((log:any) => (
+            <List.Item key={log.id}>
+              {log.totalCharged}
+            </List.Item>
+          ))}
+        </List>
     </div>
   );
 }
